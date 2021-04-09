0e6b32d3ffc6929f85ca926a8c3d1aa5248ec7be
Ryan Saunders <42478632+systemsevenfive@users.noreply.github.com>
2021-04-08 21:55:22 -0400

    Set up button to present an alert

--- RememberObjC/ViewController.m
+++ RememberObjC/ViewController.m
@@ -18,5 +18,11 @@
     // Do any additional setup after loading the view.
 }
 
+- (IBAction)clickBait:(id)sender {
+    UIAlertController *alert = [UIAlertController alertControllerWithTitle:@"Welcome" message:@"Welcome to the world of Objective-C" preferredStyle:UIAlertControllerStyleAlert];
+    UIAlertAction *closeAction = [UIAlertAction actionWithTitle:@"Close" style:UIAlertActionStyleCancel handler:nil];
+    [alert addAction:closeAction];
+    [self presentViewController:alert animated:YES completion:nil];
+}
 
 @end
